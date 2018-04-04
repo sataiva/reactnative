926d0192b0a288a46e93654f45ce23e34cfed0e0
Prakash <prakash@sataiva.com>
2018-04-04 18:00:52 +0530

    changes in side menu

--- src/pages/reactnative-intro/reactnative-navigation.jsx
+++ src/pages/reactnative-intro/reactnative-navigation.jsx
@@ -16,10 +16,52 @@ import {StackNavigator} from 'react-navigation';
 import HomeScreen from "../src/screens/Home.js";
 import ProfileScreen from "../src/screens/Profile.js";
 
+export default class AppNavigator extends React.Component {
+  render=()=>{
+    return <App/>
+  }
+}
+
 const App = StackNavigator({
     Home: { screen: HomeScreen },
     Profile: { screen: ProfileScreen }
 });
+`;
+
+const menu = `
+import React from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, ScrollView, View, Text, Image, TouchableOpacity } from "react-native";
+import { graphqlFetch } from "../utils/graphql";
+
+const window = Dimensions.get("window");
+export default class Menu extends React.Component {
+  state = {};
+  render() {
+    return (
+      <ScrollView scrollsToTop={false} style={styles.menu}>
+        <View style={styles.menucontainer}>
+          <View style={styles.menuheader}>
+            <Image style={styles.menuheaderlogo} source={require("../images/user.png")}/>
+            <Text style={styles.menuheadertext}>Name</Text>
+          </View>
+
+          <View style={styles.menuview}>
+            <TouchableOpacity style={styles.menubutton} onPress={() => { this.props.togglemenu(); this.props.navigate("Home"); }}>
+            <Text style={styles.buttonText}>Home</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.menubutton} onPress={() => { this.props.togglemenu(); this.props.navigate("Profile");}}>
+            <Text style={styles.buttonText}>Profile</Text>
+          </TouchableOpacity>
+          </View>
+          <View style={styles.footercontainer}>
+            <Text style={styles.footertext}>@2018</Text>
+          </View>
+        </View>
+      </ScrollView>
+    );
+  }
+}
 `;
 
 export default props =>
@@ -60,7 +102,30 @@ export default props =>
       by a key value pair. Using those <b> Key </b> only we can navigate through
       screens. Ex. Home, Profile.
     </p>
+    <p>
+      <small>AppNavigator.js</small>
+    </p>
     <Block value={samplestack} />
+
+    <h4>4. Create a Menu component</h4>
+    <p>
+      Here i am gonna create a component which has navigation menu names and
+      designed side menu interface.
+    </p>
+    <p>
+      <small>SideMenu.js</small>
+    </p>
+    <Block value={menu} />
+
+    <h4>4. Create a SideMenu component</h4>
+    <p>
+      Here i am gonna use a previous Menu component to display as a side menu
+      when opening menu and inject them with navigation properties
+    </p>
+    <p>
+      <small>Menu.js</small>
+    </p>
+    <Block value={menu} />
     <CtaButton to="/reactnative-intro/reactnative-signup">Previous</CtaButton>
     <CtaButton to="/reactnative-intro/">Next</CtaButton>
   </MainLayout>;
