877c3bb9245dc0f820d5db51a046382cdab0ae56
Prakash <prakash@sataiva.com>
2018-04-10 17:59:31 +0530

    changes

--- src/pages/reactnative-intro/reactnative-drawerNavigator.jsx
+++ src/pages/reactnative-intro/reactnative-drawerNavigator.jsx
@@ -5,6 +5,8 @@ import CtaButton from "../../components/CtaButton";
 import Block from "../../components/Block";
 import emptyhomeimage from "../../images/drawernavigator/emptyhome.png";
 import emptyprofileimage from "../../images/drawernavigator/emptyprofile.png";
+import newdrawerimage from "../../images/drawernavigator/newdrawer.png";
+import designedDrawer from "../../images/drawernavigator/designeddrawer.png";
 
 const emptyscreens = `
 import React from "react";
@@ -49,6 +51,69 @@ export default DrawerNavigator(
       drawerWidth: 250
     }
   );
+`;
+
+const Menu = `
+import React, { Component } from "react";
+import { NavigationActions } from "react-navigation";
+import { ScrollView, Text, View, StyleSheet } from "react-native";
+
+class SideMenu extends Component {
+
+  navigateToScreen = route => () => {
+    const navigateAction = NavigationActions.navigate({
+      routeName: route });
+    this.props.navigation.dispatch(navigateAction);
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <Text style={{ fontSize: 22, fontWeight: "600", color: "white" }}> Name </Text>
+        </View>
+        <ScrollView style={styles.scrollview}>
+          <View style={styles.menuView}>
+            <Text style={styles.menuText} onPress={this.navigateToScreen("Home")}> Home </Text>
+          </View>
+          <View style={styles.menuView}>
+            <Text style={styles.menuText} onPress={this.navigateToScreen("Profile")}> Profile </Text>
+          </View>
+        </ScrollView>
+        <View style={styles.footerContainer}>
+          <Text style={{ color: "white" }}>This is my fixed footer</Text>
+        </View>
+      </View>
+    );
+  }
+}
+export default SideMenu;
+`;
+
+const MenuWithDrawer = `
+import React from "react";
+import { View, Text, Button, StyleSheet, Image } from "react-native";
+import { DrawerNavigator } from "react-navigation";
+import SideMenu from "./Menu";
+
+class Home extends React.Component {....}
+class Profile extends React.Component {.....}
+
+export default DrawerNavigator(
+  {
+    Home: {
+      screen: Home
+    },
+    Profile: {
+      screen: Profile
+    }
+  },
+  {
+    contentComponent: SideMenu,
+    drawerWidth: 250
+  }
+);
+
 `;
 
 export default props =>
@@ -98,6 +163,44 @@ export default props =>
       object with key-value pair.
     </p>
     <Block value={simpledrawer} />
+    <img
+      src={newdrawerimage}
+      alt="Drawer screen"
+      height="50%"
+      width="50%"
+      style={{ position: "relative", left: "25%" }}
+    />
+    <p>
+      Here you can see, DrawerNavigator created a side menu drawer with screen
+      name and navigation options.When you click on the names it will redirect
+      to corresponding pages.
+    </p>
+
+    <h4>3.How to customize DrawerNavigator drawer </h4>
+    <p>
+      DrawerNavigator giving a property called "contentComponent". It is used to
+      create a user customizable component.
+    </p>
+    <p>
+      "contentComponent" accepts React component as an input and render it on
+      DrawerNavigator drawer.
+    </p>
+    <h6>First we need to create a Menu.js</h6>
+    <Block value={Menu} />
+    <h6>
+      Set Menu component as a value to DrawerNavigator property
+      "contentComponent".
+    </h6>
+    <Block value={MenuWithDrawer} />
+    <p>And the result will be like.</p>
+    <img
+      src={designedDrawer}
+      alt="Customized drawer screen"
+      height="50%"
+      width="50%"
+      style={{ position: "relative", left: "25%" }}
+    />
+    <p></p>
     <CtaButton to="/reactnative-intro/reactnative-signup">Previous</CtaButton>
     <CtaButton to="/reactnative-intro/">Next</CtaButton>
   </MainLayout>;
